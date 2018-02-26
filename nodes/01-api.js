5d1c538bb4c6b6b09585a22456b495bdde59f13d
Vicary Archangel <vicary.archangel@member.mensa.org>
2018-02-26 17:00:18 +0800

    Support auto idempotency key #1

--- nodes/01-api.js
+++ nodes/01-api.js
@@ -1,6 +1,191 @@
 // 01-api.js: Node for Stripe RESTful API requests.
 
 module.exports = function StripeApi(RED) {
+  const NUMARGS_MAP = {
+    "balance": {
+      "retrieve": 1,
+      "retrieveTransaction": 2,
+      "listTransactions": 2
+    },
+    "charges": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "capture": 2,
+      "list": 2
+    },
+    "customers": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2,
+      "createSource": 3,
+      "retrieveSource": 3,
+      "updateSource": 4,
+      "verifySource": 4,
+      "deleteSource": 3,
+      "listSources": 3,
+      "createCard": 3,
+      "retrieveCard": 3,
+      "updateCard": 4,
+      "deleteCard": 3,
+      "listCards": 3,
+      "deleteDiscount": 2
+    },
+    "disputes": {
+      "retrieve": 2,
+      "update": 3,
+      "close": 2,
+      "list": 2
+    },
+    "events": {
+      "retrieve": 2,
+      "list": 2
+    },
+    "fileUploads": {
+      "create": 2,
+      "retrieve": 2,
+      "list": 2
+    },
+    "payouts": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "list": 2,
+      "cancel": 2
+    },
+    "refunds": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "list": 2
+    },
+    "tokens": {
+      "create": 2,
+      "retrieve": 2
+    },
+    "sources": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3
+    },
+    "subscriptions": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2,
+      "deleteDiscount": 2
+    },
+    "subscriptionItems": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2
+    },
+    "coupons": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2
+    },
+    "invoices": {
+      "create": 2,
+      "retrieve": 2,
+      "retrieveLines": 2,
+      "retrieveUpcoming": 2,
+      "update": 3,
+      "pay": 2,
+      "list": 2
+    },
+    "invoiceItems": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2
+    },
+    "plans": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2
+    },
+    "accounts": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "reject": 3,
+      "list": 2,
+      "createLoginLink": 2,
+      "createExternalAccount": 3,
+      "retrieveExternalAccount": 3,
+      "updateExternalAccount": 4,
+      "deleteExternalAccount": 3,
+      "listExternalAccounts": 3
+    },
+    "applicationFees": {
+      "retrieve": 2,
+      "list": 2,
+      "createRefund": 2,
+      "retrieveRefund": 3,
+      "updateRefund": 4,
+      "listRefunds": 3
+    },
+    "countrySpecs": {
+      "retrieve": 2,
+      "list": 2
+    },
+    "recipients": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2
+    },
+    "transfers": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "list": 2,
+      "createReversal": 3,
+      "retrieveReversal": 3,
+      "updateReversal": 4,
+      "listReversals": 3
+    },
+    "orders": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "pay": 3,
+      "list": 2,
+      "returnOrder": 3
+    },
+    "products": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2
+    },
+    "orderReturns": {
+      "retrieve": 2,
+      "list": 2
+    },
+    "skus": {
+      "create": 2,
+      "retrieve": 2,
+      "update": 3,
+      "del": 2,
+      "list": 2
+    }
+  };
+
   return function StripeApiNode(config) {
     RED.nodes.createNode(this, config);
 
@@ -35,7 +220,14 @@ module.exports = function StripeApi(RED) {
         }
       }
 
-      method[1].apply(method[0], args)
+      args.length = NUMARGS_MAP[method[0]][method[1]] || 1;
+
+      // add idempotency key
+      if ( msg._msgid ) {
+        args.push(Object.assign({ idempotency_key: msg._msgid }, args.pop() || {}));
+      }
+
+      method[1].call(method[0], ...args)
         .then(response=> {
           if (!node.active) return;
 
