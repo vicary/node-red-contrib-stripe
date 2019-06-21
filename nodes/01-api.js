68c0108cbd213ae3b6ef3bdef1de05f0c4eeac85
Vicary Archangel <vicary.archangel@member.mensa.org>
2019-06-21 21:31:35 +0800

    Use async/await to skip argument checks at all.

--- nodes/01-api.js
+++ nodes/01-api.js
@@ -1,248 +1,75 @@
 // 01-api.js: Node for Stripe RESTful API requests.
 
 module.exports = function StripeApi(RED) {
-  const NUMARGS_MAP = {
-    "balance": {
-      "retrieve": 1,
-      "retrieveTransaction": 2,
-      "listTransactions": 2
-    },
-    "charges": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "capture": 2,
-      "list": 2
-    },
-    "customers": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2,
-      "createSource": 3,
-      "retrieveSource": 3,
-      "updateSource": 4,
-      "verifySource": 4,
-      "deleteSource": 3,
-      "listSources": 3,
-      "createCard": 3,
-      "retrieveCard": 3,
-      "updateCard": 4,
-      "deleteCard": 3,
-      "listCards": 3,
-      "deleteDiscount": 2
-    },
-    "disputes": {
-      "retrieve": 2,
-      "update": 3,
-      "close": 2,
-      "list": 2
-    },
-    "events": {
-      "retrieve": 2,
-      "list": 2
-    },
-    "fileUploads": {
-      "create": 2,
-      "retrieve": 2,
-      "list": 2
-    },
-    "payouts": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "list": 2,
-      "cancel": 2
-    },
-    "refunds": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "list": 2
-    },
-    "tokens": {
-      "create": 2,
-      "retrieve": 2
-    },
-    "sources": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3
-    },
-    "subscriptions": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2,
-      "deleteDiscount": 2
-    },
-    "subscriptionItems": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2
-    },
-    "coupons": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2
-    },
-    "invoices": {
-      "create": 2,
-      "retrieve": 2,
-      "retrieveLines": 2,
-      "retrieveUpcoming": 2,
-      "update": 3,
-      "pay": 2,
-      "list": 2
-    },
-    "invoiceItems": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2
-    },
-    "plans": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2
-    },
-    "accounts": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "reject": 3,
-      "list": 2,
-      "createLoginLink": 2,
-      "createExternalAccount": 3,
-      "retrieveExternalAccount": 3,
-      "updateExternalAccount": 4,
-      "deleteExternalAccount": 3,
-      "listExternalAccounts": 3
-    },
-    "applicationFees": {
-      "retrieve": 2,
-      "list": 2,
-      "createRefund": 2,
-      "retrieveRefund": 3,
-      "updateRefund": 4,
-      "listRefunds": 3
-    },
-    "countrySpecs": {
-      "retrieve": 2,
-      "list": 2
-    },
-    "recipients": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2
-    },
-    "transfers": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "list": 2,
-      "createReversal": 3,
-      "retrieveReversal": 3,
-      "updateReversal": 4,
-      "listReversals": 3
-    },
-    "orders": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "pay": 3,
-      "list": 2,
-      "returnOrder": 3
-    },
-    "products": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2
-    },
-    "orderReturns": {
-      "retrieve": 2,
-      "list": 2
-    },
-    "skus": {
-      "create": 2,
-      "retrieve": 2,
-      "update": 3,
-      "del": 2,
-      "list": 2
-    }
-  };
+  return function StripeApiNode({ method: configMethod, configNode, ...config }) {
+    RED.nodes.createNode(this, { method: configMethod, configNode, ...config });
 
-  return function StripeApiNode(config) {
-    RED.nodes.createNode(this, config);
-
-    let node = this;
+    let active = true;
+    let stripe;
 
     try {
-      node.stripe = RED.nodes.getNode(config.configNode).getInstance();
+      stripe = RED.nodes.getNode(configNode).getInstance();
     }
     catch (e) {
-      return node.error(RED._('stripe.invalid-config'), e);
+      this.error(RED._('stripe.invalid-config'), e);
+      this.status({ fill: 'red', shape: 'ring', text: 'Invalid Configuration' });
+      return;
     }
 
-    node.active = true;
-    node.method = config.method;
+    this.on('input', async msg => {
+      this.status({});
 
-    node.on('input', function(msg) {
-      let method = (node.method || msg.topic).split('.');
-      let args = [];
+      const [
+        thatName,
+        methodName,
+      ] = (configMethod || msg.topic).split('.');
+      const {
+        [thatName]: that,
+        [thatName]: { [methodName]: method } = {},
+      } = stripe;
 
-      method[0] = node.stripe[method[0]] || {};
-      method[1] = method[0][method[1]];
-
-      if ( !method[1] || typeof method[1] != 'function' ) {
-        return node.error(RED._('stripe.invalid-method', { method: node.method || msg.method }));
+      if (typeof method !== 'function') {
+        this.error(RED._('stripe.invalid-method', { method: `${thatName}.${methodName}` }));
+        this.status({ fill: 'red', shape: 'dot', text: `Method ${thatName}.${methodName} doesn't exist.` });
+        return;
       }
 
-      if ( msg.payload !== null && typeof msg.payload != 'undefined' ) {
-        args = msg.payload;
-
-        if ( !Array.isArray(args) ) {
-          args = [args];
+      const args = (() => {
+        if (msg.payload !== null && typeof msg.payload !== 'undefined') {
+          let args;
+          if (!Array.isArray(msg.payload)) {
+            args = [msg.payload];
+          } else {
+            args = msg.payload;
+          }
+          return args;
         }
-      }
 
-      args.length = NUMARGS_MAP[method[0]][method[1]] || 1;
+        return [];
+      })();
 
       // add idempotency key
-      if ( msg._msgid ) {
-        args.push(Object.assign({ idempotency_key: msg._msgid }, args.pop() || {}));
+      if (msg._msgid) {
+        const [arg0 = {}] = args;
+
+        arg0.idempotency_key = msg._msgid;
+        args[0] = arg0;
       }
 
-      method[1].call(method[0], ...args)
-        .then(response=> {
-          if (!node.active) return;
+      try {
+        const response = await that[method](...args);
 
+        if (active) {
           msg.payload = response;
-          node.send(msg);
-        })
-        .catch(err=> {
-          msg.error = err;
-
-          node.error(RED._('stripe.invalid-request'), msg);
-        });
+          this.send(msg);
+        }
+      } catch (err) {
+        msg.error = err;
+        this.error(RED._('stripe.invalid-request'), msg);
+        this.status({ fill: 'red', shape: 'dot', text: `[${err.type}] ${err.message}` });
+      }
     });
 
-    node.on('close', function() {
-      node.active = false;
-    });
+    this.on('close', () => active = false);
   };
 };
